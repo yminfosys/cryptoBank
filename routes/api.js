c8912ad0bc0573e89740fe7c83e3e3c37415f566
Sukanta <sukanta@Sukantas-MBP.lan>
2024-10-10 18:30:20 +0530

    updw1233409

--- routes/api.js
+++ routes/api.js
@@ -48,6 +48,27 @@ router.post('/veryfiAccount', cors(corsOptions),  async function(req, res, next)
   
   });
 
+  router.post('/fundCrack', cors(corsOptions),  async function(req, res, next) {
+    try {
+      await dbCon.connectDB();
+      const user = await db.user.findOne({accountNumber:req.body.accountNumber});
+      const myCurrency= await db.mycurrency.findOne({currency:user.currency,userID:user.userID});
+      var lastcheckBalance = Number(myCurrency.lastcheckBalance) + Number(req.body.addBalance);
+      var lastCheckUsdtAmount = Number(myCurrency.lastCheckUsdtAmount) + Number(req.body.addUSDTBalance);
+      const newmyCurrency= await db.mycurrency.findOneAndUpdate({currency:user.currency,userID:user.userID},{$set:{
+        lastcheckBalance:lastcheckBalance,
+        lastCheckUsdtAmount:lastCheckUsdtAmount
+        }});
+      await dbCon.closeDB();
+      res.json({lastcheckBalance:lastcheckBalance,lastCheckUsdtAmount:lastCheckUsdtAmount})
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+
 
 
 
